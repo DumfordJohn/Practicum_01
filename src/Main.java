eab70287c04695cb290dcf5bbede9a942f92d0bf
dumforjh <91568702+DumfordJohn@users.noreply.github.com>
2025-01-14 14:05:42 -0500

    Class 1/14

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        String firstName = "";
+
+        firstName = SafeInput.getNonZeroLenString(in, "Enter your first name");
+
+        System.out.println("Your first name is " + firstName);
+    }
+}
